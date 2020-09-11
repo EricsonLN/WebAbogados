da6075f439b78a2baca81ca15817b11a04a2e06c
EricsonLN <lopezericson233@gmail.com>
2020-09-10 19:50:11 -0500

    Mi primer trabajo mal hecho

---
+++ _head.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.4.2/css/all.css" integrity="sha384-/rXc/GQVaYpyDdyxK+ecHPVYJSN9bmVFBvjA/9eOB+pb3F2w2N6fc5qB9Ew5yIns" crossorigin="anonymous"/>
+    <!-- Resetear Normalize -->
+    <link rel="stylesheet" href="assets/css/normalize.css">
+    <!-- Mis estilos -->
+    
+    <link rel="stylesheet" href="assets/css/header.css">
+    <link rel="stylesheet" href="assets/css/especialidades.css">
+    <link rel="stylesheet" href="assets/css/color.css">
+    <link rel="stylesheet" href="assets/css/novedades.css">
+    <link rel="stylesheet" href="assets/css/equipo/equipo.css">
+    <link rel="stylesheet" href="assets/css/contacto.css">
+    <link rel="stylesheet" href="assets/css/footer.css">
+    <!-- Responsivos -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+    
+
+    <!-------SLIDER CARROUSEL===================================================-->
+	<link rel="stylesheet" type="text/css" href="assets/slick/slick.css">
+      <link rel="stylesheet" type="text/css" href="assets/slick/slick-theme.css">
+    <link rel="stylesheet" href="assets/css/toggle-menu.css">
+</head>
---
+++ _header.php
@@ -0,0 +1,37 @@
+<header class="main-header">
+        <div class="main-img">
+            <img src="assets/img/header-img/LogoL&L.png" alt=""/>
+        </div>
+
+        <nav class="main-menu" id="sidebar">
+            <a href="index.php">El estudio </a>
+            <a href="especialidades.php">Especialidades</a>
+            <a href="equipo.php">Equipo</a>
+            <a href="articulo-prensa.php">Novedades</a>
+            <a href="contacto.php">Contacto</a>
+        </nav>
+
+        <div class="top-bar">
+            <div class="iconmenu" id="icon-menu">
+                <i class="fas fa-bars main-menu__bar"></i>
+            </div>
+        </div>
+
+        <div class="main-contacto">
+            <p class="main-contacto__link">(51) 963082509</p>
+            <i class="fab fa-facebook-f main-contacto__icon"></i>
+            <i class="fab fa-whatsapp main-contacto__icon"></i>
+        </div>
+
+        
+    </header>
+
+    <nav class="toggle">
+            <ul class="menu2" id="main-menu2">
+                <li class="menu__item"><a class="menu__link" href="index.php">El estudio </a></li>
+                <li class="menu__item"><a class="menu__link" href="especialidades.php">Especialidades</a></li>
+                <li class="menu__item"><a class="menu__link" href="equipo.php">Equipo</a></li>
+                <li class="menu__item"><a class="menu__link" href="articulo-prensa.php">Novedades</a></li>
+                <li class="menu__item"><a class="menu__link" href="contacto.php">Contacto</a></li>
+            </ul>
+        </nav>
